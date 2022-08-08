60fda3c6574170545cf8a3bf789b24a3e7ee371d
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-08-08 10:45:24 -0400

    Java Bootcamp

---
+++ src/day01/Primitives.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        double b = 2.5;
+        char c = 'C';
+        boolean d = true;
+
+
+        byte x = 100;
+        int y = x;
+
+        short z = (short) y;
+
+        double q = 100;
+
+    }
+
+}
---
+++ src/day01/WrapperClasses.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        Integer a = 100;
+        Double b = 2.5;
+        Character c = 'c';
+        Boolean d = true;
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+       // Double g = 1000;
+
+        String str = "123";
+
+        int n1 = Integer.parseInt(str);
+
+        Integer n2 = Integer.valueOf(str);
+
+
+    }
+
+}
