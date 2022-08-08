4d8b018b5b919a3be3615880c6cb04adfd13a3c1
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-08-08 15:27:00 -0400

    Java Bootcamp

--- src/day01/ClassAndObject.java
+++ src/day01/ClassAndObject.java
@@ -4,9 +4,24 @@ import java.time.LocalDate;
 
 class Person{
 
-    public String name;
+    public String name ;
     public char gender;
     public LocalDate DOB;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, char gender) {
+        this(name);
+        this.gender = gender;
+    }
+
+    public Person(String name, char gender, LocalDate DOB) {
+        this(name, gender);
+        this.DOB = DOB;
+    }
+
 
     @Override
     public String toString() {
@@ -21,13 +36,38 @@ class Person{
 
 
 
-
 public class ClassAndObject {
 
     public static void main(String[] args) {
 
+        Person person1 = new Person("Daniel");
+        Person person2 = new Person("Ozer");
+        Person person3 = new Person("Yulia", 'F');
+        Person person4 = new Person("Ariane", 'F', LocalDate.of(1991,6, 2));
 
-        Person person1 = new Person();
+        /*
+        person1.name = "Josh";
+        person2.name = "Inci";
+        person3.name = "Bella";
+
+        person1.name = "Aaron";
+*/
+
+    //    person2.DOB = LocalDate.of(1990, 4, 7);
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+
+
+        /*
+        Person person4 = null;
+        person4.name = "James";
+        person4.gender = 'M';
+        System.out.println(person4);
+         */
+
 
     }
 
