002b8fc7936c6e015061db511e19c7579fb71ce8
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-08-08 11:51:20 -0400

    Java Bootcamp

--- src/day01/Loops.java
+++ src/day01/Loops.java
@@ -1,4 +1,32 @@
 package day01;
 
+import java.util.Scanner;
+
 public class Loops {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Europe Batch8 " + i);
+        }
+
+        System.out.println("-----------------------------------");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1~5:");
+        int num = input.nextInt();
+
+        while(num > 5 || num < 1) {
+            System.out.println("Invalid entry, please r-enter:");
+            num = input.nextInt();
+        }
+
+        System.out.println("You have selected: "+num);
+
+
+
+
+
+    }
+
 }
